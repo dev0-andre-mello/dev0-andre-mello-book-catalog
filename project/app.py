fd3ddbf91b3957d2c2f46d7989874e2e8c1c6bdc
dev0-andre-mello <punchmchn578@hotmail.com>
2025-08-04 10:13:05 -0300

    feat: Add book registration page and homepage listing with Flask templates.

--- project/app.py
+++ project/app.py
@@ -1,16 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 from models import db, Book
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///book.db"
 app.config["SQLALCHEMY_TRACK_NOTIFICATIONS"] = False
-
 db.init_app(app)
 
 
 @app.route("/")
 def index():
-    return "<h1>Catálogo de Livros</h1>"
+    books = Book.query.all()
+    return render_template("index.html", books=books)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add_book():
+    if request.method == "POST":
+        title = request.form["title"]
+        autor = request.form["autor"]
+        year = request.form["year"]
+
+        new_book = Book(title=title, autor=autor, year=year)
+        db.session.add(new_book)
+        db.session.commit()
+
+        return redirect(url_for("index"))
+    return render_template("add_book.html")
 
 
 if __name__ == "__main__":
